9994bb11722e4d5a83bb797e9ca54ca0c446c62c
Jeremy Shepherd <jeremylshepherd@gmail.com>
2015-08-03 22:46:38 +0000

    Final. Canvas resizes seamlessly up to ~80px before losing cohesion. Styled buttons to round and added hover effects.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var newDiv = function(number) {
     };
   for (i = 0; i < number; i++) {
     $('.wrapper').append('<div class="etch" />');    
-  };
+    };
 };
 
 var sketch = function(){
@@ -16,8 +16,8 @@ var sketch = function(){
 $(document).ready(function(){
   sketch();
 });
-                  
-  newDiv(4);
+
+  newDiv(16);
 
   $('#reset').on('click', function() {
     $('.etch').removeClass('sketch');
@@ -27,6 +27,11 @@ $(document).ready(function(){
     $('div').remove();
       var canvas = prompt("How many squares across?");
       newDiv(canvas);
+      var viewport = 960;
+      var gridWidth = viewport/canvas;
+      $('.etch').css({
+        width : gridWidth + 'px',
+        height: gridWidth + 'px'
+      })
       sketch();
-      });
-  
+  });
